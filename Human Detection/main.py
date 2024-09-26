9f7c9c5ec0f20a52511f3de1238b6c6bd06a6c2b
Weegeeday <simon.besner@me.com>
2024-09-25 20:05:44 -0400

    human working

--- Human Detection/main.py
+++ Human Detection/main.py
@@ -1,4 +1,20 @@
 from ultralytics import YOLO
+import cv2
 
-model = YOLO("./yolov8n.pt")  # load an official detection model
-results = model.track(source=0, show=True)  # source=0 to use the webcam
+model = YOLO("./Human Detection/yolov8n.pt")  # load an official detection model
+model.classes = [1]  # only detect human
+
+
+while True:
+    # Capture image from webcam
+    cap = cv2.VideoCapture(0)
+    ret, img = cap.read()
+    cap.release()
+    results = model(source=img, conf=0.8)
+    for det in results:
+        boxesxy = det.boxes.xyxy  # Bounding box coordinates
+        try:
+            x1, y1, x2, y2 = boxesxy[0]
+            print(f"Coordinates: {x1}, {y1}, {x2}, {y2}")
+        except IndexError:
+            print("No human detected")
