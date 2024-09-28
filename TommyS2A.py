28dd3d906bed929c8ef53bbaa092ba40db11be0f
tomtomistheone <tomtomistheone713@gmail.com>
2024-09-28 14:27:46 -0400

    why does it still have an m!

--- TommyS2A.py
+++ TommyS2A.py
@@ -1,2 +1,9 @@
+import math
+
 res_x, res_y = 640, 480
+half_x, half_y = res_x/2, res_y/2
 fov_x, fov_y = 75, 60
+def pixelToAngle(pixel, half, fov):
+    return math.degrees(math.atan( ( (pixel-half) * math.tan(math.radians(fov/2)) ) / half))
+
+print(pixelToAngle(640, half_x, fov_x))
