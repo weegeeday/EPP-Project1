2f9d6d673ccc82f888fd3755e553cc98f55c4d7c
tomtomistheone <tomtomistheone713@gmail.com>
2024-09-30 20:47:00 -0400

    improved calc func

--- TommyS2A.py
+++ TommyS2A.py
@@ -3,5 +3,7 @@ import math
 res_x, res_y = 640, 480
 half_x, half_y = res_x/2, res_y/2
 fov_x, fov_y = 75, 60
-def pixelToAngle(pixel, half, fov):
-    return round(math.degrees(math.atan( ( (pixel-half) * math.tan(math.radians(fov/2)) ) / half)))
+def pixelToAngle(pixel_x, pixel_y):
+    calc_x = round(math.degrees(math.atan( ( (pixel_x-half_x) * math.tan(math.radians(fov_x/2)) ) / half_x)))
+    calc_y = round(math.degrees(math.atan( ( (pixel_y-half_y) * math.tan(math.radians(fov_y/2)) ) / half_y)))
+    return calc_x, calc_y
